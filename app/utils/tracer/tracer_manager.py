f6a039bdae1296ab6828e67d49050a34790cadfe
Viktor Andriichuk <v.andriichuk@gmail.com>
2024-03-06 10:24:51 +0200

    Fixed issue with mypy checkings

--- app/utils/tracer/tracer_manager.py
+++ app/utils/tracer/tracer_manager.py
@@ -1,10 +1,12 @@
+from typing import Optional, Union
+
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 
-from app.utils.tracer.check_otlp_credentials import CertificateCredentialStrategy, TokenCredentialStrategy
+from app.utils.tracer.check_otlp_credentials import CertificateCredentialStrategy, TokenCredentialStrategy, CredentialStrategy
 
 
 class TraceManager:
@@ -14,7 +16,7 @@ class TraceManager:
                  insecure: bool = True,
                  use_credentials: bool = False,
                  use_ssl_certificate: bool = False,
-                 ssl_certificate_path: str = None,
+                 ssl_certificate_path: Optional[str] = None,
                  token: str = '') -> None:
         self.tracer_provider = TracerProvider(
             resource=Resource.create({SERVICE_NAME: service_name})
@@ -25,6 +27,8 @@ class TraceManager:
             credentials = None
 
             if use_credentials:
+                credential_strategy: CredentialStrategy
+
                 if use_ssl_certificate:
                     credential_strategy = CertificateCredentialStrategy(ssl_certificate_path)
                 else:
