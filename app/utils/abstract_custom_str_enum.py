16cc23662434262991e6b7ca5f52e27ac6847307
Viktor Andriichuk <v.andriichuk@gmail.com>
2024-03-13 12:32:45 +0200

    Updated abstract_custom_str_enum and delete unused method

--- app/utils/abstract_custom_str_enum.py
+++ app/utils/abstract_custom_str_enum.py
@@ -2,10 +2,5 @@ from enum import Enum
 
 
 class AbstractCustomStrEnum(str, Enum):
-    def __new__(cls, value) -> str:
-        obj = str.__new__(cls, value)
-        obj._value_ = value
-        return obj
-
     def __call__(self, *args, **kwargs) -> str:
         return self.value.format(*args, **kwargs)
